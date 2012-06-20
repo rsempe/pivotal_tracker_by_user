4abbaf44b18da39d285ef04581a674bd8778ed80
Romain Sempe <romain.sempe@gmail.com>
2012-06-20 10:43:53 +0200

    Better naming for feature methods.

--- models/feature.rb
+++ models/feature.rb
@@ -15,16 +15,16 @@ class Feature < Story
 
   ### Class Methods ###
 
-  def self.sum_points(stories, iterations)
+  def self.sum_points(features, iterations)
     points = []
-    stories.group_by(&:iteration).each { |iteration, stories| points << [stories.sum(&:points), iteration] }
+    features.group_by(&:iteration).each { |iteration, features| points << [features.sum(&:points), iteration] }
     (iterations.collect(&:first) - points.collect(&:last)).each { |point| points << [0, point] } if points.size != iterations.size
     points.sort_by { |point| point.last.to_i }
   end
 
-  def self.sum_total(stories)
+  def self.sum_total(features)
     total = []
-    Story.group_by_iteration(stories).each { |iteration, features| total << features.sum(&:points) }
+    Story.group_by_iteration(features).each { |iteration, features| total << features.sum(&:points) }
     total
   end
 
